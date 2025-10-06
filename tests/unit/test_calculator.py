33c08e5bb8cc9dab7b2f752c67b686952e43d1d5
Yashaswini717 <yashun176@gmail.com>
2025-10-06 11:59:20 +0530

    Added and Tested with negative values

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -32,5 +32,12 @@ class TestMultiplyDivideWithValidation:
         """Test divide rejects non-numeric inputs."""
         with pytest.raises(TypeError, match="Division requires numeric inputs"):
             divide("10", 2)
+    def test_subtract_negative_numbers(self):
+        assert subtract(-5, -3) == -2
+        assert subtract(-10, -4) == -6
+    def test_add_negative_numbers(self):
+        """Test adding positive numbers"""
+        assert add(-2, -3) == -5
+        assert add(-10, -15) == -25
 
 # TODO: Students will add TestMultiplyDivide class
